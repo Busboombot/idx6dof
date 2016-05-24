0dc21921d3ae94d53175701c0dc54bbab6d5af71
Phillip Steiner <phillipthomasjamessteiner@gmail.com>
2016-05-24 15:39:21 -0700

    Includes accelaration to speeds set with pots selected with joystick button

--- Accel_Test/Accel_Test.ino
+++ Accel_Test/Accel_Test.ino
@@ -10,7 +10,11 @@ int aft_stick_val[6];
 int8_t inverse_val;
 int analog_val;
 
-//uint8_t prev_dir[6] 
+int int_vel[6];
+int dst_vel[6];
+int8_t stick_accel[6] = {1,1,1}; // Accelaration Levels
+
+//uint8_t prev_dir[6]
 
 
 uint8_t dir_pins[6] = {3,5,7};
@@ -40,11 +44,11 @@ uint8_t d_velocity[6] = {0,0,0};
 #define SEL_PIN A3
 
 #define N_STICKS 3
-#define P_STICK_TOL 50
-#define N_STICK_TOL -50
+#define P_STICK_TOL 25
+#define N_STICK_TOL -25
 
 void setup() {
-  Serial.begin(115200);
+  // Serial.begin(115200);
   
   pinMode(SEL_PIN, INPUT_PULLUP);
   for (int stick = 0; stick < N_STICKS; stick ++) {
@@ -70,7 +74,7 @@ void loop() {
   for (int stick = 0; stick < N_STICKS; stick ++) { // CLK Steps
     if (!(stick_clk[stick]--)) {
       stick_clk[stick] = stick_tus[stick];
-      if (stick_tus[stick] != 0) {
+      if (stick_tus[stick] != 0 && stick_tus[stick] != 62) {
         stick_stp[stick] = 1;
       }
       else {
@@ -108,29 +112,56 @@ void readAnalog() {
     limit_values[stick] = mcp.digitalRead(limit_pins[stick]);
     sel_state = digitalRead(SEL_PIN);
     analog_val = analogRead(ANALOG_PIN(stick));
-    stick_val = map(analog_val-analog_zero[stick], -600, 600, 500, -500);
+    stick_val = map(analog_val-analog_zero[stick], -530, 530, 4000, -4000);
+    
+    // Serial.println(analog_val-analog_zero[stick]);
     
     if (sel_state == 0 && sel_bounce[stick] == 0) {
       if (stick_val > P_STICK_TOL) {
         dir = HIGH;
-        aft_stick_val[stick] = stick_val;
+        dst_vel[stick] = stick_val;
+        int_vel[stick] = aft_stick_val[stick];
       }
       else if (stick_val < N_STICK_TOL) {
         dir = LOW;
-        aft_stick_val[stick] = -1*stick_val;
+        dst_vel[stick] = -1*stick_val;
+        int_vel[stick] = aft_stick_val[stick];
       }
       else {
         dir = LOW;
-        aft_stick_val[stick] = 0;
+        dst_vel[stick] = 0;
+        int_vel[stick] = aft_stick_val[stick];
       }
       sel_bounce[stick] = 1;
     }
+    
     if (sel_state == 1) {
       sel_bounce[stick] = 0;  
     }
-    
-    Serial.println(stick_val);
-    stick_tus[stick] = aft_stick_val[stick]/100;
+    //Serial.println(dst_vel[stick]);
+    if (aft_stick_val[stick] != dst_vel[stick]) {
+      if (dst_vel[stick] > int_vel[stick]) { // Going up
+        if (aft_stick_val[stick] < dst_vel[stick]) {
+          aft_stick_val[stick] += stick_accel[stick];
+        }
+        if (aft_stick_val[stick] >= dst_vel[stick]) {
+          aft_stick_val[stick] = dst_vel[stick]; // When we hit desitination, it stays @ destination
+        }
+      }
+      if (dst_vel[stick] < int_vel[stick]) { // Going Down
+        if (aft_stick_val[stick] > dst_vel[stick]) {
+          aft_stick_val[stick] -= stick_accel[stick];
+        }
+        if (aft_stick_val[stick] <= dst_vel[stick]) {
+          aft_stick_val[stick] = dst_vel[stick];
+        }
+      }
+//      Serial.println("---");
+//      Serial.println(int_vel[stick]);
+//      Serial.println(map(aft_stick_val[stick]>>6, 0, 4000>>6, 4000>>6, 0));
+//      Serial.println(dst_vel[stick]);
+    }
+    stick_tus[stick] = map(aft_stick_val[stick]>>6, 0, 4000>>6, 4000>>6, 0);
     stick_dir[stick] = dir;
   }
 }
