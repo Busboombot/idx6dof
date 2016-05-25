0478a83cd23f89422c961e08c8ecd7ee315c6696
Phillip Steiner <phillipthomasjamessteiner@gmail.com>
2016-05-25 11:37:52 -0700

    Created New Document to allow for testing, added top and bottom functions to return all values of top or bottom. Now reads real time with  inverted position values...

---
+++ Pendant_Test/Pendant_Test.ino
@@ -0,0 +1,40 @@
+/*
+
+ */
+
+
+#include "idx_pendant.h"
+
+IDXPendant pendant;
+
+void setup() {
+
+  pendant.setup();
+
+}
+
+
+void loop() {
+  int sis = 0;
+  
+  if(pendant.run_once()) {
+    pendant.print_serial();
+
+    if (pendant.sw_pos(IDX_SW_SPEED) == IDX_SW_POS_TOP){
+      Serial.println("Fast");
+    } else if (pendant.sw_pos(IDX_SW_SPEED) == IDX_SW_POS_MID){
+      Serial.println("Medium");
+    } else if (pendant.sw_pos(IDX_SW_SPEED) == IDX_SW_POS_BOTTOM){
+      Serial.println("Slow");
+    }
+  
+    if (pendant.sw_pos(IDX_SW_AXIS0) == IDX_SW_POS_TOP){
+      Serial.println("Increase");
+    } else if (pendant.sw_pos(IDX_SW_AXIS0) == IDX_SW_POS_MID){
+      Serial.println("No Change");
+    } else if (pendant.sw_pos(IDX_SW_AXIS0) == IDX_SW_POS_BOTTOM){
+      Serial.println("Decrease");
+    }
+  }
+  
+}
---
+++ Pendant_Test/idx_pendant.h
@@ -0,0 +1,68 @@
+/*
+ *  Class for an IDX Robot Control Pendant interface
+ */
+
+#ifndef  idx_pendant_h
+#define idx_pendant_h
+
+#include "Arduino.h"
+#include <stdint.h>
+
+#define IDX_SW_SPEED 1
+#define IDX_SW_TOOL 2
+#define IDX_SW_AXIS0 3
+#define IDX_SW_AXIS1 4
+#define IDX_SW_AXIS2 5
+#define IDX_SW_AXIS3 6 
+#define IDX_SW_AXIS4 7
+#define IDX_SW_AXIS5 8 
+#define IDX_SW_TOOL_OPEN 9 
+#define IDX_SW_TOOL_ON 10 
+#define IDX_SW_TOOL_DETACH 11
+#define IDX_SW_VAC_ON 12
+#define IDX_SW_WHERE 13
+#define IDX_SW_SET 14
+#define IDX_SW_HOME 15
+#define IDX_SW_STEP 16
+#define IDX_SW_AXIS 17
+
+#define IDX_SW_POS_TOP 0
+#define IDX_SW_POS_MID 1
+#define IDX_SW_POS_BOTTOM 2
+
+class IDXPendant {
+
+  public:
+
+  IDXPendant();
+
+  static const int num_switches;
+
+  void setup(); // Initialize the input and output pins
+  
+  bool run_once(); // Scan once and record which switches are set
+  
+  void print_serial(); // Write the two switch bit values to the serial port. 
+
+  int sw_pos(int switch_n); // Return the position of a switch
+
+  private:
+
+    static const int outpins[];
+    static const int inpins[];
+
+    unsigned long swbits_ups; // bit mask of switches that are in the up position
+    unsigned long swbits_downs; // bit mask of switches that are in the down position
+    unsigned long last_swbits_ups; 
+    unsigned long last_swbits_downs; 
+    
+    uint8_t switch_set_count; // number of switches set per loop 
+    uint8_t last_switch_set_count;
+
+    bool serial_is_setup;
+
+  
+};
+
+#endif
+
