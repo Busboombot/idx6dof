31f93b23859b642f9aab520e58de87ca2e64c418
Phillip Steiner <phillipthomasjamessteiner@gmail.com>
2016-05-25 15:17:44 -0700

    Added Stepper Control

--- Pendant_Test/Pendant_Test.ino
+++ Pendant_Test/Pendant_Test.ino
@@ -1,8 +1,4 @@
-/*
-
- */
-
-
+#include "digitalWriteFast.h"
 #include "idx_pendant.h"
 
 IDXPendant pendant;
@@ -12,39 +8,38 @@ int dst_s_vel[6];
 
 uint8_t accel_clk = 0;
 
+uint8_t stick_dir[6];
+uint8_t stick_tus[6] = {5,5,5};
+uint8_t stick_clk[6];
+uint8_t stick_stp[6];
+
 #define H_DEST_VEL 200
-#define M_DEST_VEL 100
-#define L_DEST_VEL 50
+#define M_DEST_VEL 185
+#define L_DEST_VEL 135
 
 #define N_STICKS 6
-#define ACCEL 1 // Inversly Porportional
+#define ACCEL 2 // Inversly Porportional
 
+uint8_t dir_pins[6] = {3,5,7};
+uint8_t stp_pins[6] = {2,4,6};
+#define STEP_PIN(stick) stp_pins[stick]
+#define DIR_PIN(stick) dir_pins[stick]
+#define MAX_TUS 1
 #define S_SNAP_TOL 5
 
 void setup() {
-
   pendant.setup();
-  Serial.begin(115200);
+  for (int stick = 0; stick < N_STICKS; stick ++) {
+    pinMode(STEP_PIN(stick), OUTPUT);
+    pinMode(DIR_PIN(stick), OUTPUT);
+  }
+  // Port Manipulations
 }
 
 
 void loop() {
   get_dest_vel();
   
-  /*
-  if(pendant.run_once()) {
-    Serial.print("Bits Up: ");
-    Serial.print(pendant.return_up(), BIN);
-    Serial.print("--------Bits Down: ");
-    Serial.print(pendant.return_down(), BIN);
-    Serial.println("");
-  }
-  pendant.run_once();
-  for (int button = 1; button < pendant.num_switches; button ++) {
-    Serial.print(pendant.sw_pos(button));
-  }
-  Serial.println("");
-  */
   if (accel_clk == 0) {
     accel();
   }
@@ -55,7 +50,40 @@ void loop() {
     accel_clk = 0;
   }
   
-  print_vel();
+  dir_vel_set();
+  
+  for (int stick = 0; stick < N_STICKS; stick ++) { // CLK Steps
+    if (!(stick_clk[stick]--)) {
+      stick_clk[stick] = stick_tus[stick];
+      if (stick_tus[stick] != 0 && stick_tus[stick] < 50) {
+        stick_stp[stick] = 1;
+      }
+      else {
+        stick_stp[stick] = 0;
+      }
+    }
+    else {
+      stick_stp[stick] = 0;
+    }
+  }
+  for (int stick = 0; stick < N_STICKS; stick ++) { // Write Steps and Directions
+    if (stick_stp[stick] == 1) {
+      digitalWrite(STEP_PIN(stick), HIGH);
+    }
+    else if(stick_stp[stick] == 0) {
+      digitalWrite(STEP_PIN(stick), LOW);
+    }
+    
+    digitalWrite(DIR_PIN(stick), stick_dir[stick]);
+    
+    delayMicroseconds(12);
+    
+    for (int i = 0; i < N_STICKS; i ++) {
+      digitalWrite(STEP_PIN(stick), LOW);
+    }
+    
+    delayMicroseconds(12);
+  }
 }
 
 
@@ -105,9 +133,19 @@ void accel() {
   }
 }
 
-void print_vel() {
+void dir_vel_set() {
   for (int stick = 0; stick < N_STICKS; stick ++) {
-    Serial.println(s_vel[stick]);
+    if (s_vel[stick] > 0) {
+      stick_dir[stick] = HIGH;
+      stick_tus[stick] = map(s_vel[stick], 0, 200, 50, MAX_TUS);
+    }
+    else {
+      stick_dir[stick] = LOW;
+      stick_tus[stick] = map(s_vel[stick], 0, -200, 50, MAX_TUS);
+    }
   }
-  Serial.println("----------");
+}
+
+void portWrite (int8_t port, int8_t state) {
+  
 }
