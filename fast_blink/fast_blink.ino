8a1c62ac3728cb23b4da7313769370e5532abb8b
Phillip Steiner <phillipthomasjamessteiner@gmail.com>
2016-05-26 13:17:16 -0700

    Defined for Arduino Due with a delay between states

--- fast_blink/fast_blink.ino
+++ fast_blink/fast_blink.ino
@@ -2,17 +2,18 @@
 #include "pins_arduino.h"
 
 
-#define fastSet(pin) ( digitalPinToPort(pin)->PIO_SODR |= digitalPinToBitMask(pin) ) 
-#define fastClear(pin) ( digitalPinToPort(pin)->PIO_CODR |= digitalPinToBitMask(pin) ) 
+#define fastSet(pin) (digitalPinToPort(pin)->PIO_SODR |= digitalPinToBitMask(pin) ) 
+#define fastClear(pin) (digitalPinToPort(pin)->PIO_CODR |= digitalPinToBitMask(pin) ) 
 
 void setup() {
   pinMode(13, OUTPUT);
 }
 
 void loop() {
-
-  fastSet(13);
-  fastClear(13);
-  fastSet(13);
-  fastClear(13);
+  #if defined (__arm__) && defined (__SAM3X8E__)
+    fastSet(13);
+    delay(250);
+    fastClear(13);
+    delay(250);
+  #endif
 }
