01b04c4a594670085b2d2711a88393e98984725c
Eric Busboom <eric@clarinova.com>
2016-05-27 10:39:02 -0700

    .

---
+++ limit_test/limit_test.ino
@@ -0,0 +1,17 @@
+
+#include "idx_pendant.h"
+#include "idx_limit.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+
+  IDXMcpLimit l;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
