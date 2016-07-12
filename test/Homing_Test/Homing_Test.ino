363e553a2ecbb547f43a7af21ba25ca3647b08f6
Phillip Steiner <phillipthomasjamessteiner@gmail.com>
2016-07-12 13:06:43 -0700

    Added in developed Quad Counter

--- test/Homing_Test/Homing_Test.ino
+++ test/Homing_Test/Homing_Test.ino
@@ -1,3 +1,8 @@
+#include "digitalWriteFast.h"
+#include "Arduino.h"
+#include "direct_pin_read.h"
+#include "interrupt_pins.h"
+
 #define STEP_PIN 6
 #define DIR_PIN 7
 #define LIMIT_PIN 5
@@ -7,12 +12,20 @@
 
 #define BCK_LSH 100
 
-uint16_t encCount;
+typedef struct {
+  volatile IO_REG_TYPE *pin1_register;
+  volatile IO_REG_TYPE *pin2_register;
+  IO_REG_TYPE pin1_bitmask;
+  IO_REG_TYPE pin2_bitmask;
+  volatile uint8_t state;
+  volatile int8_t direction;
+  volatile int32_t position; // Hmmmn, why aren't these volatile?
+} Encoder_internal_state_t;
+
+Encoder_internal_state_t encoder;
+
 uint16_t LimIndxDist;
 volatile uint8_t indexTrig;
-
-uint8_t quadDir;
-volatile uint8_t quadState = 0;
 
 int8_t vel;
 uint8_t stepClk;
@@ -29,8 +42,8 @@ ISR (PCINT1_vect) {
 
 void setup() {
 
-  attachInterrupt(digitalPinToInterrupt(QUAD_A), QuadPulseA, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(QUAD_B), QuadPulseB, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(QUAD_A), update_quadrature, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(QUAD_B), update_quadrature, CHANGE);
   
   for (uint8_t i=A0; i<=A5; i++) {
     pinMode(i, INPUT);
@@ -42,7 +55,16 @@ void setup() {
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
 
-  vel = 10; // Set Velocity to 5 cycles per step
+  encoder.pin1_register = PIN_TO_BASEREG(QUAD_A);
+  encoder.pin1_bitmask = PIN_TO_BITMASK(QUAD_A);
+  encoder.pin2_register = PIN_TO_BASEREG(QUAD_B);
+  encoder.pin2_bitmask = PIN_TO_BITMASK(QUAD_B);
+
+  encoder.position = 0;
+  encoder.state = 0;
+  encoder.direction = 0;
+  
+  vel = 5; // Set Velocity to 5 cycles per step
   
   findHome(1);
 }
@@ -138,17 +160,16 @@ void findHome(uint8_t dir) { // Function Tailored for Elbow 1 Axis
   }
   
   indexTrig = 0;
-  encCount = 0; 
+  encoder.position = 0; 
   
   while (distFound == 0) {
-    Serial.println(encCount);
     switch (dir) {
       case 0:
         // Step CounterClockwise
         vel = abs(vel)*-1;
         step();
         if (indexTrig != 0) {
-          LimIndxDist = abs(encCount);
+          LimIndxDist = abs(encoder.position);
           distFound = 1;
         }
         break;
@@ -157,7 +178,7 @@ void findHome(uint8_t dir) { // Function Tailored for Elbow 1 Axis
         vel = abs(vel);
         step();
         if (indexTrig != 0) {
-          LimIndxDist = abs(encCount);
+          LimIndxDist = abs(encoder.position);
           distFound = 1;
         }
         break;
@@ -186,32 +207,70 @@ void step() {
 
 
 
-void QuadPulseA() {
-  AddCount();
-  if (quadState == 1 && digitalRead(QUAD_A) == HIGH) {
-    quadDir = 0;
-  }
-  if (digitalRead(QUAD_A) == HIGH) {
-    quadState = 0;
-  }
+void read_initial_encoder_state(){
+    delayMicroseconds(2000);
+    uint8_t s = 0;
+    if (DIRECT_PIN_READ(encoder.pin1_register, encoder.pin1_bitmask)) {
+      s |= 1;
+    }
+    if (DIRECT_PIN_READ(encoder.pin2_register, encoder.pin2_bitmask)) {
+      s |= 2;
+    }
+    encoder.state = s;
 }
 
-void QuadPulseB() {
-  AddCount();
-  if (quadState == 0 && digitalRead(QUAD_B) == HIGH) {
-    quadDir = 1;
-  }
-  if (digitalRead(QUAD_B) == HIGH) {
-    quadState = 1;
-  }
-}
+void update_quadrature() {
 
-void AddCount() {
-  if (quadDir == 0) {
-    encCount--;
+  /// Code originally from: Paul Stoffregen <paul@pjrc.com>
+
+  uint8_t p1val = DIRECT_PIN_READ(encoder.pin1_register, encoder.pin1_bitmask);
+  uint8_t p2val = DIRECT_PIN_READ(encoder.pin2_register, encoder.pin2_bitmask);
+
+  // Keep only the bottom two bits, the last values for A and B
+  uint8_t state = encoder.state & 3;
+  // Set the current tw values for A and B above the old ones.
+  if (p1val){
+    state |= 4;
   }
-  else {
-    encCount++;
+  if (p2val){
+    state |= 8;
+  }
+
+
+  // For the next round, shift the old values off.
+  encoder.state = (state >> 2);
+
+  switch (state) {
+    case 1:  // 1  = B0001
+    case 7:  // 7  = B0111
+    case 8:  // 8  = B1000
+    case 14: // 14 = B1110
+      encoder.position ++;
+      encoder.direction = 1;
+      return;
+    case 2:  // 2  = B0010
+    case 4:  // 4  = B0100
+    case 11: // 11 = B1011
+    case 13: // 13 = B1101
+      encoder.position--;
+      encoder.direction = -1;
+      return;
+    // These are cases when only one pin has an interrupt
+    case 3:  // 3  = B0011
+    case 12: // 12 = B1100
+      encoder.position += 2;
+      encoder.direction = 1;
+      return;
+    case 6: // 6 = B0110
+    case 9: // 9 = B1001
+      encoder.position -= 2;
+      encoder.direction = -1;
+      return;
+      // Unused, impossible states.
+      // 0
+      // 5  = B0101
+      // 10 = B1010
+      // 15 = B1111
   }
 }
 
