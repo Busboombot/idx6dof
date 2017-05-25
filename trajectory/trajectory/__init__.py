49f0a6f041a6a8d1394065aa1341ba33322d87a0
Eric Busboom <eric@clarinova.com>
2017-05-25 07:47:01 -0700

    .

--- trajectory/trajectory/__init__.py
+++ trajectory/trajectory/__init__.py
@@ -2,4 +2,5 @@
 from segments import SegmentList, SegmentBuffer, Segment, JointSegment, SegmentIterator
 from sim import SimSegment
 from proto import Proto, Command, Response
-from joystick import Joystick
+from joystick import PygameJoystick
+from .util import freq_map
