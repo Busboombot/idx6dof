5b889d27b32e2c36e5ab578ae405206a81572506
Eric Busboom <eric@busboom.org>
2021-08-14 17:46:47 -0700

    .

--- PowerConroller/PowerConroller.ino
+++ PowerConroller/PowerConroller.ino
@@ -7,9 +7,127 @@
   Released into the public domain.
 */
 
+#include "U8glib.h"
+
+
+
+#define NBUTTONS 4
 
 #define pt(x) Serial.print(x)
 #define pl(x) Serial.println(x)
+#define ps Serial.print(' ')
+
+
+class FadeLed {
+
+  private:
+    int _pin;
+
+    int ledState = LOW;             // ledState used to set the LED
+  
+    unsigned long lastBlinkTime = 0;        // will store last time LED was updated
+    unsigned long lastFadeTime = 0;        // will store last time LED was updated
+    
+
+    const static int DEFAULT_BLINK_INT = 1000;   
+    const static int DEFAULT_FADE_INT = 50; 
+    const static int DEFAULT_FADE_SPEED = 5; 
+
+    int blinkInterval = DEFAULT_BLINK_INT;
+    int fadeInterval = DEFAULT_FADE_INT;
+    int fadeAmount = DEFAULT_FADE_SPEED;    // how many points to fade the LED by
+    int fadeMax = 255;
+
+    
+    int brightness = 0;    // how bright the LED is 
+
+  public:
+    int mode = FadeLed::BLINK;
+
+  public:
+
+  const static int ON = 1;
+  const static int OFF = 2;
+  const static int BLINK = 3;
+  const static int FADE = 4; 
+
+  FadeLed(int pin) :_pin(pin){
+    pinMode(_pin, OUTPUT);
+    
+  }
+
+  void setFade(int speed=DEFAULT_FADE_SPEED, int max=255){
+    mode = FadeLed::FADE;
+    fadeAmount = speed;
+   
+    fadeMax = max;
+  }
+
+  void setFadeInterval(int intv){
+    fadeInterval = intv;
+  }
+
+  void setBlink(int interval=DEFAULT_BLINK_INT){
+    blinkInterval = interval;
+    mode = FadeLed::BLINK;
+  }
+
+  void setOn(){
+    mode = FadeLed::ON;
+  }
+
+  void setOff(){
+    mode = FadeLed::OFF;
+  }
+  
+  void update(){
+
+
+    switch (mode){
+      case  FadeLed::ON: {
+        digitalWrite(_pin, HIGH);
+        break;
+      }
+      case  FadeLed::OFF: {
+        digitalWrite(_pin, LOW);
+        break;
+      }
+      case  FadeLed::FADE: {
+        if (millis() - lastFadeTime >= fadeInterval) {
+          // save the last time you blinked the LED
+          lastFadeTime = millis();
+    
+          brightness = brightness + fadeAmount;
+      
+          if (brightness <= 0 || brightness >= fadeMax) {
+            fadeAmount = -fadeAmount;
+            if (brightness <= 0 ){
+              brightness = 0;
+            } else{
+              brightness = fadeMax;
+            }
+          }
+
+          analogWrite(_pin, brightness);
+        }
+        break;
+      }
+      case  FadeLed::BLINK: {
+        if (millis() - lastBlinkTime >= blinkInterval) {
+          // save the last time you blinked the LED
+          lastBlinkTime = millis();
+          ledState = !ledState;
+        }
+
+        digitalWrite(_pin, ledState);
+
+        break;
+      }  
+    }
+  }
+  
+};
+
 
 class Button {
   private:
@@ -17,17 +135,17 @@ class Button {
     int _pin;
     unsigned long _delay;
     unsigned long _lastDebounceTime;
-    int _lastStateBtn;
-    bool _setState; // The  button state tht is considered "set"
-    bool _wasSet; // Flagged when button is True
-    bool _wasUnset; // Flagged when button is True
+    int _lastStateBtn = HIGH;
+    bool _setState = false; // The  button state tht is considered "set"
+    bool _wasSet = false; // Flagged when button is True
+    bool _wasUnset = false; // Flagged when button is True
 
     bool isTimeToUpdate(){
       return (millis() - _lastDebounceTime) > _delay;
     }
 
   public:
-    Button(String label, int pin, bool setState = true, unsigned long delay=250) : 
+    Button(String label, int pin, bool setState = true, unsigned long delay=125) : 
       _label(label), _pin(pin), _delay(delay), _setState(setState) {
       pinMode(pin, INPUT_PULLUP);
       _lastDebounceTime = 0;
@@ -87,17 +205,23 @@ class Button {
       return ws;
       
     }
+
+    int checkStateChange(){
+
+      if (checkSetAndClear()){
+        return 1;
+      } else if (checkUnsetAndClear()){
+        return 0;
+      } else {
+        return -1;
+      }
+      
+    }
+    
     
     int stateChanged(){}
 };
 
-#define NBUTTONS 4
-Button buttons[NBUTTONS] = {
-  Button("reset",12, false),
-  Button("key",11, false),
-  Button("estop",10, false),
-  Button("power",9, false)
-};
 
 
 class StateMachine {
@@ -105,97 +229,295 @@ class StateMachine {
   private: 
     static const int OFF_STATE = 10;
 
-    void (StateMachine::*current_state)() = &StateMachine::offState;
-   
-    Button buttons[NBUTTONS] = {
-      Button("reset",12, false),
-      Button("key",11, false),
-      Button("estop",10, false),
-      Button("power",9, false)
-    };
+    void (StateMachine::*current_state)() = &StateMachine::initState;
 
-    bool checkReset(){ return buttons[0].checkSetAndClear(); }
-    bool checkKeyOn(){ return buttons[1].state()==true; }
+    int lowPowerPin; // Enable 48 Volt power supplies
+    int highPowerPin; // Enable 80 Volt power supplies
+
+    FadeLed led;
+   
+    Button buttons[NBUTTONS];
+
+    String stateLabel;
+
+    #define SB_LEN 100
+    char str_buf[SB_LEN];
+    
+
+    bool stateInit = false; // Has the new state been initialized?
+
+    bool checkReset() { return buttons[0].state()==true; }
+    bool checkKeyOn() { return buttons[1].state()==true; }
     bool checkKeyOff(){ return buttons[1].state()==false; }
-    bool checkEStop(){ return buttons[2].checkSetAndClear(); }
-    bool checkPower(){ return buttons[3].checkSetAndClear(); }
+    bool checkEStop() { return buttons[2].state()==true; }
+    bool checkPower() { return buttons[3].state()==true; }
 
     bool checkAbort(){
       return checkKeyOff() | checkEStop();
     }
-    
-    void offState() {}
-    void lowPowerOnState() {}
-    void highPowerOnState() {}
-    void errorState(){
 
-      if (checkReset()){
+    void initState(){
+      stateLabel = "Init";
+      
+    }
+    
+    void offState() {
+      stateLabel = "off";
+
+      if (!stateInit){
+        turnOffHighPower();
+        turnOffLowPower();
+        led.setOff();
+        stateInit=true;
         
+      } else {
+        
+        if (checkKeyOn()){   
+          toOnState();  
+        }
+      }
+    }
+
+    void onState() {
+      stateLabel = "on";
+
+      if (!stateInit){
+        turnOffHighPower();
+        turnOffLowPower();
+        led.setFadeInterval(50);
+        led.setFade(10);
+        stateInit=true;
+        
+      } else {
+        if (checkKeyOff()){
+          toOffState();
+        }  else if (checkReset()){
+          toOnState();
+        }  else if (checkPower()){
+          toFullPowerOnState();  
+        }
+      }
+    }
+    
+    void lowPowerOnState() {
+      stateLabel = "lowPower";
+
+      if (!stateInit){
+        turnOffHighPower();
+        turnOnLowPower();
+        led.setBlink(1000);
+        stateInit=true;
+      } else {    
+        if(checkKeyOff()){
+          toOffState();  
+        } else if (checkPower()){
+          toFullPowerOnState();
+        } else if (checkReset()){
+          toOnState();
+        } 
+
       }
       
     }
-    void shuttingDownState(){}
-
-    // Transition to other states
-    void toOffState() {
-      current_state = &StateMachine::offState;
+    void fullPowerOnState()  {
+      stateLabel = "fullPower";
+      
+      if (!stateInit){
+        turnOnHighPower();
+        turnOnLowPower();
+        led.setOn();
+        stateInit=true;
+      } else {    
+        if(checkKeyOff()){
+          toOffState();  
+        } else if ( checkPower()){
+          toLowPowerOnState();
+        } else if(checkEStop() ){
+          toErrorState();
+        }  else if (checkReset()){
+          toOnState();
+        } 
+      }
+      
     }
-    void toLowPowerOnState()  {
-      current_state = &StateMachine::lowPowerOnState;
+    
+    void errorState(){
+      stateLabel = "error";
+      
+      if (!stateInit){
+        turnOffHighPower();
+        
+        led.setBlink(125);
+        stateInit=true;
+      } else {    
+        if(checkKeyOff()){
+          toOffState();  
+        } else if ( checkPower()){
+          toLowPowerOnState();
+        }  else if (checkReset()){
+          toOnState();
+        } 
+      }
+      
     }
-    void toHighPowerOnState()  {
-      current_state = &StateMachine::highPowerOnState;
-    }
-    void toErrorState() {
-      current_state = &StateMachine::errorState;
-    }
-    void toShuttingDownState() {
-      current_state = &StateMachine::shuttingDownState;
+    
+    void shuttingDownState(){
+      stateLabel = "shutdown";
     }
 
+    void setRunCurrentState(void (StateMachine::*new_current_state)()){
+      current_state = new_current_state;
+      
+      stateInit = false;
+      // Initialize the state
+      execState();
+      pt("New State:"); ps; pl(stateLabel);
+     
+    }
 
-    void turnOnLowPower(){}
-    void turnOnHighPower(){}
-    void turnOffLowPower(){}
-    void turnOffHighPower(){}
-
-
+    void turnOnLowPower(){
+      digitalWrite(lowPowerPin, HIGH);
+    }
+    void turnOnHighPower(){
+      digitalWrite(highPowerPin, HIGH);
+    }
+    void turnOffLowPower(){
+      digitalWrite(lowPowerPin, LOW);
+    }
+    void turnOffHighPower(){
+      digitalWrite(highPowerPin, LOW);
+    }
 
     
   public:
 
-    StateMachine(){
-      
+    StateMachine(int ledPin, int powerPin, int estopPin, int keyPin, int resetPin, 
+                 int highPowerPin, int lowPowerPin) : 
+      led(ledPin), 
+      buttons( {Button("reset",resetPin, false),Button("key",keyPin, false),Button("estop",estopPin, false),Button("power",powerPin, false)}),
+      highPowerPin(highPowerPin), lowPowerPin(lowPowerPin) {
+
+      pinMode(highPowerPin, OUTPUT);
+      pinMode(lowPowerPin, OUTPUT);
+
+      updateButtons();
+     
     }
 
-    void update(){
+    // Transition to other states
+    void toOnState() {
+      setRunCurrentState(&StateMachine::onState);
+    }
+      
+    void toOffState() {
+      setRunCurrentState(&StateMachine::offState);
+    }
+    void toLowPowerOnState()  {
+      setRunCurrentState(&StateMachine::lowPowerOnState);
+    }
+    void toFullPowerOnState()  {
+      setRunCurrentState(&StateMachine::fullPowerOnState);
+    }
+    void toErrorState() {
+      setRunCurrentState(&StateMachine::errorState);
+    }
+    void toShuttingDownState() {
+      setRunCurrentState(&StateMachine::shuttingDownState);
+    }
+
+    void execState(){
+      (((StateMachine*)this)->*StateMachine::current_state) ();
+    }
+
+    void buttonsChanged(){
+
+       printButtons();
+
+        // Possibly transition states
+        execState();
+       
+    }
+
+    bool updateButtons(){
+      bool stateChanged = false;      
+      
       for(int i = 0; i < NBUTTONS; i++){
         buttons[i].update();
+        if ( buttons[i].checkStateChange() != -1){
+          stateChanged = true;
+        }
       }
-   
+
+      return stateChanged;
     }
-    
+
+    bool update(){
+
+      bool stateChanged = updateButtons();
+
+      led.update();
+
+      if(stateChanged){
+        buttonsChanged();
+      }
+
+      return stateChanged;
+    }
+
+    void printButtons(){
+       pt("State: "); pt(stateLabel);pt(" Buttons: "); ps;
+        for(int i = 0; i < NBUTTONS; i++){
+          pt(buttons[i].label());pt(":");ps;pt(buttons[i].state());ps;
+        }
+        pl();
+    }
 
 
-  
+    char* getStateLabel(){
+      stateLabel.toCharArray(str_buf, SB_LEN);
+      return str_buf;
+    }
 };
+
+
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE);  // I2C / TWI 
+
+StateMachine sm(3,9, 10, 11, 12, 43, 45) ;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting...");
   Serial.print(NBUTTONS);
   Serial.println(" buttons");
+  sm.printButtons();
+  sm.toOffState();
 
+
+  
+ 
+  u8g.setFont(u8g_font_unifont);
+  
+}
+
+void _draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  u8g.drawStr( 0, 22, sm.getStateLabel());
+}
+
+void draw(void) {
+  u8g.firstPage();  
+  do {
+    _draw();
+  } while( u8g.nextPage() );
 }
 
 void loop() {
-  for(int i = 0; i < NBUTTONS; i++){
-    buttons[i].update();
 
-    if (buttons[i].checkSetAndClear()){
-      pt(buttons[i].state()); pt(' ');
-      pl(buttons[i].label());
-    }
+  
+  if(sm.update()){
+    draw();
   }
-
+  
 }
